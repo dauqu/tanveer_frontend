04bfeba511f58a534f749804e85ee3b1859b5000
dauqu <sujeetsingh734321@gmail.com>
2023-01-18 16:45:13 +0530

    live link

--- src/components/Constant.js
+++ src/components/Constant.js
@@ -1,2 +1,2 @@
-// export const API = "https://grocery.dauqu.host";
-export const API = "http://localhost:4000";
+export const API = "https://grocery.dauqu.host";
+// export const API = "http://localhost:4000";
